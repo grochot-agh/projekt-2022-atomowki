272bfa483ef0f6f30afa86fa8e595b73c0c6bfb5
mpiwko01 <100602506+mpiwko01@users.noreply.github.com>
2022-06-12 22:38:38 +0200

    Add files via upload

---
+++ users.sql
@@ -0,0 +1,5 @@
+INSERT INTO `users` (`imienazwisko`, `login`, `haslo`) VALUES
+('Aneta Bruzda', 'anetabruzda', 'aneta123'),
+('Magdalena Piwko', 'magdapiwko', 'magda123'),
+('Patrycja Misterka', 'patrycjamisterka', 'patrycja123'),
+('Julia Wójtowicz', 'juliawojtowicz', 'julia123');
