218061d40900b854fd6e8dd3c44fb0b163042ae4
anetabruzda <100602572+anetabruzda@users.noreply.github.com>
2022-06-13 11:54:18 +0200

    Update sklep.php

--- sklep.php
+++ sklep.php
@@ -14,9 +14,10 @@ session_start();
             integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
             crossorigin="anonymous"
         />
-        <script src="JS/jquery-3.5.1.min.js"></script>
-        <script src="JS/multislider.min.js"></script>
-        <title></title>
+        <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+        <script src="jquery-3.5.1.min.js"></script>
+        <script src="multislider.min.js"></script>
+        <title>Sklep</title>
     </head>
 
     <body>
